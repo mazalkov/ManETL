930adc4b7d6ef1e75bb46dcc1fed2ea5d03076ce
Andrew <32670729+anzdrew@users.noreply.github.com>
2023-06-05 15:53:32 +0100

    Adding click

--- python/main.py
+++ python/main.py
@@ -1,8 +1,13 @@
 from arcticdb import Arctic
 import streamlit as st
 import pandas as pd
+import click
 
 
 data = pd.read_csv("data/AAPL.csv")
 
 st.dataframe(data.head(10))
+
+@click.command()
+def etl():
+    pass
