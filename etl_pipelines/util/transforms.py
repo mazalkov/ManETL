aaae396ac8c728518348638f20aaa36f2c1f47bf
ahodnett <abby_hodnett@hotmail.com>
2023-06-08 09:54:22 +0100

    adding changes to transfroms.py file

--- etl_pipelines/util/transforms.py
+++ etl_pipelines/util/transforms.py
@@ -4,6 +4,9 @@ import math
 import matplotlib.pyplot as plt
 import yfinance as yf
 
+#IMA_df = pd.read_csv('data/AAPL.csv')
+
+#IMA_df['daily_returns']=(IMA_df['Close'].pct_change())*100
 
 calcs = {
     "calc_VWAP": calc_VWAP,
@@ -13,6 +16,7 @@ calcs = {
 def calc_VWAP(high, low, close):
     typical_price = np.mean([high, low, close])
     return typical_price
+
 
 # VWAP = Cumulative Typical Price x Volume/Cumulative Volume
 # Where Typical Price = High price + Low price + Closing Price/3
@@ -25,8 +29,22 @@ def calc_daily_returns(Close, Volume):
 
 
 #IMA_df['VWAP'] = IMA_df.apply(lambda x: VWAP(x["High"], x["Low"], x["Close"]), axis=1)
-IMA_df = pd.read_csv('data/AAPL.csv')
-IMA_df['daily_returns']=(IMA_df['Close'].pct_change())*100
 
 
+def calculate_volatility(IMA_df):
+    daily_volatility = IMA_df['Daily_Return'].std()
+    monthly_volatility = math.sqrt(21) * daily_volatility
+    annual_volatility = math.sqrt(252) * daily_volatility
+    
+    print('Daily volatility:')
+    print('Apple: {:.2f}%'.format(daily_volatility))
+    
+    print('Monthly volatility:')
+    print('Apple: {:.2f}%'.format(monthly_volatility))
+    
+    print('Annual volatility:')
+    print('Apple: {:.2f}%'.format(annual_volatility))
+
+# Assuming IMA_df contains the DataFrame with daily returns column 'daily_returns'
+calculate_volatility(IMA_df)
 
