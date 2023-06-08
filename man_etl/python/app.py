74e5471f4e0cf5dd74fab86c303c9ddb239e4922
Atikah <atikah.hussain@hotmail.co.uk>
2023-06-08 18:56:26 +0100

    Add basic interactive widgets to app.py

--- man_etl/python/app.py
+++ man_etl/python/app.py
@@ -1,8 +1,26 @@
 import streamlit as st
+# import pyarrow_client
+import yfinance as yf
 
-import pyarrow_client
+# table = pyarrow_client.get_data()
+# st.dataframe(table)
 
+INTERVAL = "1h"
+PERIOD = "1d"
+TICKERS = ["AAPL", "GOOG", "TSLA"]
+YF_DATA = dict()
+for ticker in TICKERS:
+    YF_DATA[ticker] = yf.Ticker(ticker)
 
-table = pyarrow_client.get_data()
+st.title("Man ETL Dashboard")
 
-st.dataframe(table)
+if "chosen_ticker" not in st.session_state:
+    st.session_state["chosen_ticker"] = TICKERS[0]
+
+st.selectbox("Choose ticker:", key="chosen_ticker", options=TICKERS)
+
+ticker = st.session_state["chosen_ticker"]
+data = YF_DATA[ticker].history(period=PERIOD, interval=INTERVAL)
+st.write(f"Volume data for {YF_DATA[ticker].info['longName']} for {PERIOD}")
+st.line_chart(data["Volume"])
+
