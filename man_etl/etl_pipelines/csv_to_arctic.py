27fbc14e3c4f5d7fb440e8dc64595e012212575e
Sharleen Hussey <sharleenhussey@hotmail.com>
2023-06-08 17:21:52 +0100

    fix

--- man_etl/etl_pipelines/csv_to_arctic.py
+++ man_etl/etl_pipelines/csv_to_arctic.py
@@ -2,19 +2,16 @@ import logging
 from typing import List, Dict
 from logging import getLogger
 import click
-from man_etl.etl_pipelines.core.arctic import ArcticInitializer, ArcticStorer
+from man_etl.etl_pipelines.core.arctic import ArcticInitializer
+from man_etl.etl_pipelines.core.storers import ArcticStorer
 from man_etl.etl_pipelines.core.extractors import CSVExtractor, YFExtractor
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger('Logger')
 
-
 CSV_PATH = "../data/"
 DB_PATH = "lmdb:////mnt/c/Users/sharl/repos/DTS/BigData/WM9L8-IMA/astore"
 S3_PATH = "s3://s3.eu-west-2.amazonaws.com:manstocks?region=eu-west-2&access=AKIAVHAD6ZB4RYHDPBWA&secret=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"
-
-
-
 
 @click.command()
 @click.option("--library", prompt="Enter library name", default="test")
