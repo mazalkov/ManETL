6185b38de691abaf1987b686834b745b6a542ffe
Andrew <32670729+anzdrew@users.noreply.github.com>
2023-06-08 17:58:40 +0100

    Pyarrow client is now working, can extract parquet from local Pyarrow server

--- man_etl/python/pyarrow_client.py
+++ man_etl/python/pyarrow_client.py
@@ -1,4 +1,24 @@
 import pyarrow as pa
 import pyarrow.flight
+import pandas as pd
 
-client = pa.flight.connect("grpc://0.0.0.0:8815")
+try:
+    client = pa.flight.connect("grpc://localhost:8818")
+except Exception as e:
+    print(f"Failed to connect to the server: {e}")
+    exit(1)
+
+try:
+    upload_descriptor = pyarrow.flight.FlightDescriptor.for_path("ons/cpi.parquet")
+    resource_flight = client.get_flight_info(upload_descriptor)
+except Exception as e:
+    print(f"Failed to get flight info: {e}")
+    exit(1)
+
+try:
+    reader = client.do_get(resource_flight.endpoints[0].ticket)
+    read_table = reader.read_all()
+    print(read_table.to_pandas().head())
+except Exception as e:
+    print(f"Failed to read data: {e}")
+    exit(1)
