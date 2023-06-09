872f2bf8116a437f54f921c24f0bb8eebabf6d5f
Atikah <atikah.hussain@hotmail.co.uk>
2023-06-09 10:24:32 +0100

    Remove yfinance code, replace with arctic code

--- man_etl/python/app.py
+++ man_etl/python/app.py
@@ -1,26 +1,30 @@
 import streamlit as st
 # import pyarrow_client
-import yfinance as yf
+from arcticdb import Arctic
 
 # table = pyarrow_client.get_data()
 # st.dataframe(table)
 
-INTERVAL = "1h"
-PERIOD = "1d"
-TICKERS = ["AAPL", "GOOG", "TSLA"]
-YF_DATA = dict()
-for ticker in TICKERS:
-    YF_DATA[ticker] = yf.Ticker(ticker)
+## Load data from arctic
+
+bucket = Arctic("s3://s3.eu-west-2.amazonaws.com:manstocks?region=eu-west-2&access=AKIAVHAD6ZB4RYHDPBWA&secret=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")
+lib = bucket['etl_demo']
+raw_symbols = [symbol for symbol in lib.list_symbols() if '_TRANSFORMED' not in symbol]
+transformed_symbols = [df_name for df_name in lib.list_symbols() if '_TRANSFORMED' in df_name]
+transformed_dfs_dict = dict(zip(raw_symbols, [lib.read(df_name).data for df_name in transformed_symbols]))
+
+## Streamlit UI
 
 st.title("Man ETL Dashboard")
 
-if "chosen_ticker" not in st.session_state:
-    st.session_state["chosen_ticker"] = TICKERS[0]
+if "chosen_symbol" not in st.session_state:
+    st.session_state["chosen_symbol"] = raw_symbols[0]
 
-st.selectbox("Choose ticker:", key="chosen_ticker", options=TICKERS)
+st.selectbox("Choose symbol:", key="chosen_symbol", options=raw_symbols)
 
-ticker = st.session_state["chosen_ticker"]
-data = YF_DATA[ticker].history(period=PERIOD, interval=INTERVAL)
-st.write(f"Volume data for {YF_DATA[ticker].info['longName']} for {PERIOD}")
-st.line_chart(data["Volume"])
+
+chosen_symbol = st.session_state["chosen_symbol"]
+st.write(f"Volume data for {chosen_symbol}")
+st.line_chart(transformed_dfs_dict[chosen_symbol]["Volume"])
+
 
